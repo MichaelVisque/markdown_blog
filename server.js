18ab7ce1acdacbd059e0da7ff605582bc188a561
VisqueII <58799509+VisqueII@users.noreply.github.com>
2021-01-23 13:55:11 -0800

    Changed \ require(dotnev).parse\ to \ require(dotnev).config\

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
 const express = require('express')
-const mongoose = require('mongoose')
 const Article = require ('./models/article')
 const articleRouter = require('./routes/articles')
 const methodOverride = require('method-override')
 const app = express()
 
-mongoose.connect('mongodb://localhost/blog', {
+/* mongoose.connect('mongodb://localhost/blog', {
     useNewUrlParser: true, useUnifiedTopology: true,  useCreateIndex: true
-})
+}) */
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
@@ -18,6 +21,15 @@ app.get('/', async (req, res) => {
     res.render('articles/index', { articles: articles})
 })
 
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true, useUnifiedTopology: true,  useCreateIndex: true
+})
+
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
+
 app.use('/articles', articleRouter)
 
-app.listen(5000)
+app.listen(process.env.PORT || 5000)
